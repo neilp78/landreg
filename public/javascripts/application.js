db84b65ec3e4e63ce2979f02fe89bad63f807162
“neilp” <n.phillips@kainos.com>
2018-07-12 10:51:52 +0100

    filter working

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,5 +6,43 @@ if (window.console && window.console.info) {
 }
 
 $(document).ready(function () {
-  window.GOVUKFrontend.initAll()
+  window.GOVUKFrontend.initAll();
+
+  $(".fixed-view-downloads").hide();
+  $(".add-to-download").click(function() {
+    var thisText = $(this).parent().find('h2').text();
+    //console.log(thisText);
+    $('.fixed-view-downloads span').text(thisText);
+    $(".fixed-view-downloads").fadeIn();
+  });
+  $(".closethis").click(function() {
+    $(".fixed-view-downloads").hide();
+  });
+
+  $('.filter-user-type').each(function() {
+    //$( this ).addClass( "foo" );
+    var gotoPersonal = 'createaccount-yourdetails';
+    var gotoUK = 'createaccount-uk';
+    var gotoOversea = 'createaccount-overseas';
+
+    var thisradio = $(this).find('.govuk-radios__item input');
+    $('#user-select input').on('change', function() {
+       var target = ($(this).val());
+
+       switch(target) {
+            case 'personal':
+                $('.govuk-form-group a').attr('href', gotoPersonal);
+                break;
+            case 'uk-organisation':
+                alert(target);
+                $('.govuk-form-group a').attr('href', gotoUK);
+                break;
+            case 'overseas-organisation':
+                alert(target);
+                $('.govuk-form-group a').attr('href', gotoOversea);
+                break;
+        }
+       //$(updatedLink).attr('href',target)
+    });
+  });
 })
