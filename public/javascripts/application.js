8851817f58c8d229d630e45ff964de6e4c5a36a9
“neilp” <n.phillips@kainos.com>
2018-12-04 14:54:56 +0000

    and again

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 // Warn about using the kit in production
-if (window.console && window.console.info) {
+/*if (window.console && window.console.info) {
   //window.console.info('GOV.UK Prototype Kit - do not use for production')
   //session
   if (typeof(Storage) !== "undefined") {
@@ -9,7 +9,7 @@ if (window.console && window.console.info) {
   } else {
       document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
   }
-}
+} */
 
 
 
