f0b5eb8674ca1c7d4955a6fe8ef8eeb78c33dca2
Eilis van der Merwe <eilis@vandermerwe.co.za>
2018-12-13 10:15:16 +0000

    fixing again..

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
     sessionStorage.setItem("dataset", currentDataSet);
 
   }
+  console.log(sessionStorage.getItem("dataset"));
 
   switch(sessionStorage.getItem("dataset")) {
       case '-registered-leases-':
@@ -48,25 +49,22 @@ $('.state-2 a').click(function() {
   $('.state-3').show();
   event.preventDefault();
 });
+$('.state-1 .back-a-step').click(function(){
 
-$('.cancel-this').click(function(){
+});
+$('.state-2 .back-a-step').click(function(){
   $('.state-1').show();
   $('.state-3').hide();
   $('.state-2').hide();
-  $(".download-wrapper :checkbox").prop('checked', false); // Unchecks it
-  $('.download-wrapper .govuk-button').addClass('disabled_btn');
 });
-
-$('.download-wrapper .govuk-button').unbind( "click" );
+$('.state-3 .back-a-step').click(function(){
+  $('.state-1').hide();
+  $('.state-2').show();
+  $('.state-3').hide();
+});
 $(".download-wrapper :checkbox").bind('change', function(){
   //alert('selected');
   $('.download-wrapper .govuk-button').removeClass('disabled_btn');
-  //$('.download-wrapper .govuk-button').bind( "click" );
-
-  $('.state-2 .govuk-button').click(function(){
-    $('.state-2').hide();
-    $('.state-3').show();
-  });
 });
 //Get data attribute of prototype
 $('.context').each(function(){
@@ -317,7 +315,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
