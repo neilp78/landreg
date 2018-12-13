3f651c592108b554370c7584da7be156f1350b8a
Eilis van der Merwe <eilis@vandermerwe.co.za>
2018-12-13 10:30:25 +0000

    fixing JS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -325,7 +325,7 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modal, close i
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
